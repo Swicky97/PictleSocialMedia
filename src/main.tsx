09135044d0c1754d1cbb6869fd68589531ae05d9
Chase Swick <chaseswick@gmail.com>
2024-10-11 14:50:14 -0400

    Tailwind setup and base files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
-  </StrictMode>,
 )
