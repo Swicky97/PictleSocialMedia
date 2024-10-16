0cb792cd80cbb29782e26137815b86d34be66c5b
Chase Swick <chaseswick@gmail.com>
2024-10-16 19:15:19 -0400

    Initial routing

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SinginForm = () => {
+const SigninForm = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const SinginForm = () => {
   )
 }
 
-export default SinginForm
+export default SigninForm
