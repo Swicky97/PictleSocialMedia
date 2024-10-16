06ba2847343a09c8b59eab6e6f63ccbf4d65e96b
Chase Swick <chaseswick@gmail.com>
2024-10-16 19:59:04 -0400

    Signup screen and button import

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 )
