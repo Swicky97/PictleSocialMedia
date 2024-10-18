d3620704c788b431d95131286f8db5f05cc79da1
Chase Swick <chaseswick@gmail.com>
2024-10-18 03:59:17 -0400

    Sign in auth

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,4 +1,4 @@
-import { ID } from 'appwrite';
+import { ID, Query } from 'appwrite';
 
 import { INewUser } from "@/types";
 import { account, appwriteConfig, avatars, databases } from './config';
@@ -24,20 +24,22 @@ export async function createUserAccount(user: INewUser) {
             imageUrl: avatarUrl
         })
 
-        return newAccount;
+        return newUser;
     } catch (error) {
         console.log(error);
         return error;
     }
 }
 
-export async function saveUserToDB(user: {
-    accountId: string;
-    email: string;
-    name: string;
-    imageUrl: URL;
-    username?: string;
-}) {
+export async function saveUserToDB(
+    user: {
+        accountId: string;
+        email: string;
+        name: string;
+        imageUrl: URL;
+        username?: string;
+    }) {
+
     try {
         const newUser = await databases.createDocument(
             appwriteConfig.databaseId,
@@ -51,3 +53,34 @@ export async function saveUserToDB(user: {
         console.log(error);
     }
 }
+
+export async function signInAccount(user: { email: string; password: string }) {
+    try {
+        const session = await account.createSession(user.email, user.password);
+  
+        return session;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getCurrentUser() {
+    try {
+        const currentAccount = await account.get();
+    
+        if (!currentAccount) throw Error;
+    
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [Query.equal("accountId", currentAccount.$id)]
+        );
+  
+        if (!currentUser) throw Error;
+    
+        return currentUser.documents[0];
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+  }
